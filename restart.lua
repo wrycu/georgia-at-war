222cb817fa9876c858a7550d7b57709fb02931b3
Wrycu <leetsaber@gmail.com>
2020-08-12 20:52:32 -0700

    fix(mission): night/morning and weather support * correct timing for restart

--- restart.lua
+++ restart.lua
@@ -1,6 +1,6 @@
 function early_warning_check()
     local early_warning_time = 60 * 160 -- 2 hours 40 minutes
-    local early_warning_time = 60 * 1 -- 2 hours 40 minutes
+    --local early_warning_time = 60 * 1 -- 2 hours 40 minutes
     if os.time() - game_state["startup_time"] >= early_warning_time then
         trigger.action.outText("THE MISSION WILL BE RESTARTING IN 20 MINUTES!  YOU SHOULD PROBABLY START WORKING ON GETTING BACK TO BASE.", 30)
         game_state["startup_time"] = os.time()
@@ -12,7 +12,7 @@ end
 
 function warning_check()
     local warning_time = 60 * 15 -- 15 minutes
-    local warning_time = 60 * 2 -- 15 minutes
+    --local warning_time = 60 * 2 -- 15 minutes
     if os.time() - game_state["startup_time"] >= warning_time then
         trigger.action.outText("THE MISSION IS RESTARTING IN 5 MINUTES.", 30)
         game_state["startup_time"] = os.time()
@@ -24,7 +24,7 @@ end
 
 function restart_check()
     local restart_time = 60 * 5 -- 5 minutes
-    local restart_time = 60 * 3 -- 5 minutes
+    --local restart_time = 60 * 3 -- 5 minutes
     if os.time() - game_state["startup_time"] >= restart_time then
         trigger.action.setUserFlag(1337, true)
     else
